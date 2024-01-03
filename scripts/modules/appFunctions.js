4bda6dc216597f1388174e281dd2034f315fb83c
Sara Moad <102942360+SaraMoad@users.noreply.github.com>
2024-01-03 15:07:54 +1100

    Revert "Modals"

--- scripts/modules/appFunctions.js
+++
@@ -1,9 +0,0 @@
-const addTextElement = (elementType, text, parent, cls) => {
-  const newElement = document.createElement(elementType);
-  const newText = document.createTextNode(text);
-  newElement.classList.add(cls);
-  newElement.appendChild(newText);
-  parent.appendChild(newElement);
-};
-
-export { addTextElement };
